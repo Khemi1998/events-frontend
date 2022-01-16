76dbdc144e7ee9e1f1d5edfb703bcd56a3de1524
Khemi <khemi@Khemis-Laptop.local>
2022-01-16 16:30:30 +0000

    add filteredcard component

---
+++ src/components/FilteredCard/FilteredCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./FilteredCard.scss"
+
+export const FilteredCard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FilteredCard;
