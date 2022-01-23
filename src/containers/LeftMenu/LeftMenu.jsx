b9354496c18be1a796b2182e3af96f7c9f55a57c
Khemi <khemi@Khemis-Laptop.local>
2022-01-23 18:52:17 +0000

    working on color schemes

--- src/containers/LeftMenu/LeftMenu.jsx
+++ src/containers/LeftMenu/LeftMenu.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./LeftMenu.scss";
 import Form from "../../components/Form/Form";
-import sunrise from "../../assets/images/sunrise.png";
-import sun from "../../assets/images/sun.png";
-import moon from "../../assets/images/moon.png";
 import Button from "../../components/Button/Button";
 import { useState } from "react/cjs/react.development";
 
@@ -12,16 +9,13 @@ export const LeftMenu = (props) => {
   const [showForm, setShowForm] = useState(false);
 
   const currentHour = new Date().getHours();
-  let greetingImg = sunrise;
   let greetingTime = "Morning";
 
   if (currentHour >= 12) {
-    greetingImg = sun;
     greetingTime = "Afternoon";
   }
 
   if (currentHour >= 18) {
-    greetingImg = moon;
     greetingTime = "Evening";
   }
 
@@ -31,10 +25,7 @@ export const LeftMenu = (props) => {
 
   return (
     <div className="leftmenu">
-      <header className="greeting">
-        <img src={greetingImg} className="greeting__img" alt={greetingTime} />
         <h1 className="greeting__heading">Good {greetingTime}</h1>
-      </header>
       <div className="leftmenu__btns">
         <Button
           click={toggleAllEvents}
